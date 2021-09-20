fc3dd7e940cca4ec77bb197a1d2b14f5c66c46be
Vertigo <vertigomaill@gmail.com>
2021-09-20 23:49:12 +0300

    add Layout, styles

---
+++ components/Layout/Layout.js
@@ -0,0 +1,3 @@
+import styles from './Layout.module.css';
+
+export const Layout = ({ children }) => <div className={styles.container}>{children}</div>;
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,23 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
+import { Layout } from '../../components';
 
-export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a href="/">Back to home</a>
-        </Link>
-      </h2>
-    </>
-  );
-}
+const FirstPost = () => (
+  <Layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <h1>First Post</h1>
+
+    <Image src="/images/vertigo.jpg" width={300} height={400} />
+
+    <h2>
+      <Link href="/">
+        <a href="/">Back to home</a>
+      </Link>
+    </h2>
+  </Layout>
+);
+
+export default FirstPost;
