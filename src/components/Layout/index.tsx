05a58789170586f0a39ed83bc70a06d3262c011f
Vertigo <vertigomaill@gmail.com>
2021-10-29 00:19:07 +0300

    chore: add paths to tsconfig, fix eslint

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import utilStyles from '@styles/utils.module.css';
+import utilStyles from '@/styles/utils.module.css';
 import styles from './styles.module.scss';
 
 const name = 'My Name';
