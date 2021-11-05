4431d5e471fd5d8187f7a162769d482cf694033d
Vertigo <vertigomaill@gmail.com>
2021-11-05 18:56:58 +0300

    chore: rework posts index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
-import Link from 'next/link';
 
-import { Date } from '@/components/Date';
 import { Layout } from '@/components/Layout';
+import { PostsList } from '@/components/PostsList';
 import { getSortedPostsData } from '@/lib/posts';
 import { PagePropsType } from '@/types/posts';
 import { siteTitle } from '@/constants/constants';
 import utilStyles from '@/styles/utils.module.css';
+
+import styles from './styles.module.scss';
 
 export async function getStaticProps() {
   const postsData = getSortedPostsData();
@@ -24,23 +25,9 @@ export default function Home({ postsData }: PagePropsType) {
         <title>{siteTitle}</title>
       </Head>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {postsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-
-              <br />
-
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${styles.posts}`}>
+        <h2 className={utilStyles.headingLg}>Posts</h2>
+        <PostsList postsData={postsData} />
       </section>
     </Layout>
   );
